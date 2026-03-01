f1fa6d915ea083118d2a53bcf3c74d9162fafb16
MishkaKovalQA <mishka.koval.qa@gmail.com>
2026-03-01 17:01:38 +0500

    add daysteps structure and methods

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,13 +1,60 @@
 package daysteps
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/personaldata"
+	"github.com/Yandex-Practicum/tracker/internal/spentenergy"
+)
+
 type DaySteps struct {
-	// TODO: добавить поля
+	Steps    int
+	Duration time.Duration
+	personaldata.Personal
 }
 
 func (ds *DaySteps) Parse(datastring string) (err error) {
-	// TODO: реализовать функцию
+	slice := strings.Split(datastring, ",")
+	if len(slice) != 2 {
+		return fmt.Errorf("invalid input format: expected 2 parts, got %d: %w", len(slice), err)
+	}
+
+	steps, err := strconv.Atoi(slice[0])
+	if err != nil {
+		return fmt.Errorf("invalid steps format: %w", err)
+	}
+	if steps <= 0 {
+		return fmt.Errorf("steps must be greater than zero: %w", err)
+	}
+
+	duration, err := time.ParseDuration(slice[1])
+	if err != nil {
+		return fmt.Errorf("invalid duration format: %w", err)
+	}
+	if duration <= 0 {
+		return fmt.Errorf("duration must be greater than zero: %w", err)
+	}
+
+	ds.Steps = steps
+	ds.Duration = duration
+
+	return nil
 }
 
 func (ds DaySteps) ActionInfo() (string, error) {
-	// TODO: реализовать функцию
+	distance := spentenergy.Distance(ds.Steps, ds.Height)
+
+	calories, err := spentenergy.WalkingSpentCalories(ds.Steps, ds.Weight, ds.Height, ds.Duration)
+	if err != nil {
+		return "", fmt.Errorf("error calculating calories: %w", err)
+	}
+
+	result := fmt.Sprintf("Количество шагов: %d.\n", ds.Steps)
+	result += fmt.Sprintf("Дистанция составила %.2f км.\n", distance)
+	result += fmt.Sprintf("Вы сожгли %.2f ккал.\n", calories)
+
+	return result, nil
 }
