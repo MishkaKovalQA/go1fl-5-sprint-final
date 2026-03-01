4dff5e48f0c73fd3e7eff7ff1a72339ac949b63f
MishkaKovalQA <mishka.koval.qa@gmail.com>
2026-03-01 17:21:48 +0500

    fix after tests

--- internal/trainings/trainings.go
+++ internal/trainings/trainings.go
@@ -72,6 +72,6 @@ func (t Training) ActionInfo() (string, error) {
 		return "", ErrUnsupportedTrainingType
 	}
 
-	return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f",
+	return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n",
 		t.TrainingType, t.Duration.Hours(), distance, meanSpeed, calories), nil
 }
